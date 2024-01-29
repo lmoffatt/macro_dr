063ccfd1a1c2a9e3312793dd1eec39162d8fbdbb
Luciano Moffatt <lmoffatt@gmail.com>
2024-01-29 13:23:32 -0300

    slurm scripts for MH_Li  #59

--- multi_task/MH_Li7_sim.sh
+++ multi_task/MH_Li7_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-case $(($SLURM_TASK_ID % 4)) in
+case $(($SLURM_TASK_ID%4)) in
 0)
 /home/lmoffatt/Code/macro_dr/build-macro_dr-gcc-Release/macro_dr ../macro_dr/models/scheme_1.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
 ;;
