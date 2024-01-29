b7e652446acd4977f33579c9d04270271b7848ef
Luciano Moffatt <lmoffatt@gmail.com>
2024-01-29 15:11:17 -0300

    slurm scripts for MH_Li  #59

--- multi_task/MH_Li7_sim.sh
+++ multi_task/MH_Li7_sim.sh
@@ -3,19 +3,19 @@
 
 case $SLURM_LOCALID in
 0)
-/home/lmoffatt/Code/macro_dr/build-macro_dr-gcc-Release/macro_dr ../macro_dr/models/scheme_1.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_1.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
 ;;
 
 1)
-/home/lmoffatt/Code/macro_dr/build-macro_dr-gcc-Release/macro_dr ../macro_dr/models/scheme_2.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_2.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
 ;;
 
 2)
-/home/lmoffatt/Code/macro_dr/build-macro_dr-gcc-Release/macro_dr ../macro_dr/models/scheme_3.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_3.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
 ;;
 
 3)
-/home/lmoffatt/Code/macro_dr/build-macro_dr-gcc-Release/macro_dr ../macro_dr/models/scheme_4.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_4.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_sim\"" ../macro_dr/scripts/evidence_simulation.txt
 ;;
 
 esac
