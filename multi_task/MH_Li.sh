08046519c8b76f68a05b5068c917323da58ea3f8
Luciano Moffatt <lmoffatt@gmail.com>
2024-01-29 17:50:06 -0300

    run full experiment in Serafin #60

---
+++ multi_task/MH_Li.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+case $SLURM_LOCALID in
+0)
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_1_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_sim\"" ../macro_dr/scripts/evidence_data.txt
+;;
+
+1)
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_2_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_sim\"" ../macro_dr/scripts/evidence_data.txt
+;;
+
+2)
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_3_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_sim\"" ../macro_dr/scripts/evidence_data.txt
+;;
+
+3)
+/home/lmoffatt/macro_dr/macro_dr/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_sim\"" ../macro_dr/scripts/evidence_data.txt
+;;
+
+esac
+
