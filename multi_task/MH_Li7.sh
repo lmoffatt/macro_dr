1a98984fe1e4d1dc16cddca0b38938c7c16ccc35
Luciano Moffatt <lmoffatt@gmail.com>
2024-01-31 23:00:34 -0300

    number of scouts per ensemble 32 v2

--- multi_task/MH_Li7.sh
+++ multi_task/MH_Li7.sh
@@ -2,19 +2,19 @@
 
 case $SLURM_LOCALID in
 0)
-/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_1.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7\"" ../macro_dr/scripts/evidence_data.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_1.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_v2\"" ../macro_dr/scripts/evidence_data.txt
 ;;
 
 1)
-/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_2.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7\"" ../macro_dr/scripts/evidence_data.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_2.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_v2\"" ../macro_dr/scripts/evidence_data.txt
 ;;
 
 2)
-/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_3.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7\"" ../macro_dr/scripts/evidence_data.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_3.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_v2\"" ../macro_dr/scripts/evidence_data.txt
 ;;
 
 3)
-/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_4.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7\"" ../macro_dr/scripts/evidence_data.txt
+/home/lmoffatt/macro_dr/macro_dr/macro_dr  ../macro_dr/models/scheme_4.txt ../macro_dr/scripts/experiment_7.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li7_v2\"" ../macro_dr/scripts/evidence_data.txt
 ;;
 
 esac
