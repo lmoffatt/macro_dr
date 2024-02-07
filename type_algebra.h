545e41c84dff27bfd9158015ec7f08406a1a92ae
Luciano Moffatt <lmoffatt@gmail.com>
2024-02-07 08:19:52 -0300

    FuncMap_St method append (of all possibilities of a templated function) is implemented #86 #88

--- type_algebra.h
+++ type_algebra.h
@@ -3,6 +3,7 @@
 
 
 #include "maybe_error.h"
+#include <functional>
 #include <type_traits>
 #include <utility>
 #include <variant>
@@ -61,41 +62,65 @@ template<class... T> struct S{
     
     
     template<class T2>
-    friend auto operator*( S, S<T2>)
+        requires (!is_of_this_template_type_v<T2,P>)
+    friend auto operator*( S<T2>, S)
     {
-        return (S<>{}+...+P<T,T2>{});
-    }
-    template<class... T2>
-    friend auto operator*( S, S<T2...>)
-    {
-        return (S<>{}+...+(S{}*S<T2>{}));
-    }
-    template<class... T2>
-    friend auto operator*( S, std::variant<T2...>)
-    {
-        return (S<>{}+...+(S{}*S<T2>{}));
+        return (S<>{}+...+P<T2,T>{});
     }
     
+    template<class... T2>
+    friend auto operator*( S<P<T2...>>, S)
+    {
+        return (S<>{}+...+P<T2...,T>{});
+    }
+    template<class... T2>
+    friend auto operator*(S<T2...>,S)
+    {
+        return (S<>{}+...+(S<T2>{}*S{}));
+    }
+   
 };
 }
 
 template<auto x>
-struct V{};
+struct V{
+   constexpr static decltype(x) value=x; 
+};
+
+
+template<template<class...>class>
+struct Co{
+};
+
+
 
 
 template<class...T0>
-
-auto prod(std::variant<T0...>const & x)
+auto prod_variant_of_tuples_and_variant(std::variant<T0...>const & x)
+{
+    return x;
+}
+template<class...T0>
+auto sum_variant_type(std::variant<T0...>const & x)
 {
     return x;
 }
 
-template<class...T0, class... T1>
-             
-auto prod_variant(std::tuple<T0...>const & x,const std::variant<T1...>& y)
+template<class...T0, class... T1, class... Vs>
+requires              (is_of_this_template_type_v<Vs,std::variant>&&...)
+auto sum_variant_type(std::variant<T0...>const & x, std::variant<T1...>const &y,const Vs&...vs)
 {
+    return sum_variant_type(std::variant<T0...,T1...>{},vs...);
+}
+
+template<class...T0, class... T1>
+            
+auto prod_tuple_variant(std::tuple<T0...>const & x,const std::variant<T1...>& y)
+{
+   // using m7=typename decltype(y)::dentro_prod_tuple_variant;    
     return std::visit([&x](auto & e)->std::variant<std::tuple<T0...,T1>...>
-                      { return std::apply([&e](auto&...t){return std::tuple(e,t...);},x);},y);
+                      { return std::apply([&e](auto&...t){return std::tuple(t...,e);},x);
+   },y);
 }
 
 
@@ -104,33 +129,83 @@ template<class...Tu, class... T1, class...Vs>
     requires ((is_of_this_template_type_v<Tu,std::tuple>&&...)&&
              (is_of_this_template_type_v<Vs,std::variant>&&...)
              )
-auto prod_variant(std::variant<Tu...>const & x,const std::variant<T1...>& y, const Vs&...vs)
+auto prod_variant_of_tuples_and_variant(std::variant<Tu...>const & x,const std::variant<T1...>& y, const Vs&...vs)
 {
-    return std::visit([&y, &vs...](auto& ex)->std::variant<std::decay_t<decltype(prod_variant(prod_variant(Tu{},y),vs...))>...>
-                      {return prod_variant(prod_variant(ex,y),vs...);},x);    
+     auto next_x=std::visit([&y](auto& tu)
+             {
+              auto xy=prod_tuple_variant(tu,y);
+              using variant_all=std::decay_t<decltype(sum_variant_type(prod_tuple_variant(Tu{},y)...))>;
+              return std::visit([](auto e){return variant_all(e);},xy);}
+                             ,x
+                   );
+     return prod_variant_of_tuples_and_variant(next_x,vs...);    
 }
+
+
+
+
+template<class... T1>
+std::variant<std::tuple<T1>...> variant_to_variant_of_tuples(const std::variant<T1...>& y)
+{
+    return std::visit([](auto& e)->std::variant<std::tuple<T1>...>
+                      {return std::tuple(e);},y);
+}
+
+
+template< class... T1, class...Vs>
+    requires (is_of_this_template_type_v<Vs,std::variant>&&...)
+             
+auto prod_variant(const std::variant<T1...>& y, const Vs&...vs)
+{
+    return prod_variant_of_tuples_and_variant(variant_to_variant_of_tuples(y),vs...);    
+}
+
+template<class F,class...T>
+auto Apply_tuples(F&& f,std::tuple<T...> const&x)
+{
+   return std::apply([&f](auto&...ts){return f(ts...);},x);
+}
+
 
 template<class F,class...Tus>
     requires (is_of_this_template_type_v<Tus,std::tuple>&&...)
-auto Apply_variant(F&& f,std::variant<Tus ...> const&x)
+auto Apply_variant_of_tuples(F&& f,std::variant<Tus ...> const&x)
 {
-    return std::visit([&f](auto& tu)->std::variant<std::decay_t<
-                                         decltype(std::apply([f](auto&...ts){return f(ts...);},Tus{}))>...>
-                      {
-        return std::apply([f](auto&...ts){return f(ts...);},tu);},x);
-}
+    using return_type=std::variant<std::decay_t<
+        decltype(Apply_tuples(std::forward<F>(f),Tus{}))>...>;
+    
+    
+    
+    return std::visit([&f](auto& tu){
+        return return_type(Apply_tuples(f,tu));}
+                      ,x);
+  }
 
 
 template<class F,class...Vs>
     requires (is_of_this_template_type_v<Vs,std::variant>&&...)
 auto Apply_variant(F&& f,std::tuple<Vs ...> const&x)
 {
-    auto variant_of_tuples=std::apply([](auto&...e){return prod_variant(e...);},x);
+    auto variant_of_tuples=std::apply([](auto&...e)
+       {return prod_variant(e...);},x);
+    //using m5=typename decltype(variant_of_tuples)::despues_variant_of_tuples;  
     
-    return Apply_variant(std::forward<F>(f),variant_of_tuples);      
+    return Apply_variant_of_tuples(std::forward<F>(f),variant_of_tuples);      
 }
 
+namespace safe_visitor{
 
+template<class F, class... Ts>
+auto safe_visit(F&&f, std::variant<Ts...>const &x)
+{
+    using return_type=std::variant<std::invoke_result_t<F,Ts const&>...>;
+    return std::visit([f](auto const& e)
+                      { return return_type(std::invoke(std::forward<F>(f),e));},x);
+        
+}    
+
+
+}
 
 
 
