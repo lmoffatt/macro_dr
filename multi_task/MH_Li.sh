85d085491933e6f93d570d3c72b873ef939a5f6a
Luciano Moffatt <lmoffatt@gmail.com>
2024-02-24 07:54:38 -0300

    run thermo evidence in tupac v7  #108

--- multi_task/MH_Li.sh
+++ multi_task/MH_Li.sh
@@ -3,19 +3,19 @@
 
 case $SLURM_LOCALID in
 0)
-$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_1_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v6\"" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_fraction_data.txt
+$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_1_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v7\"" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
 ;;
 
 1)
-$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_2_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v6\"" ../macro_dr/scripts/likelihood.txt  ../macro_dr/scripts/evidence_fraction_data.txt
+$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_2_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v7\"" ../macro_dr/scripts/likelihood.txt  ../macro_dr/scripts/evidence_thermo_data.txt
 ;;
 
 2)
-$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_3_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v6\"" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_fraction_data.txt
+$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_3_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v7\"" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
 ;;
 
 3)
-$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v6\"" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_fraction_data.txt
+$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_v7\"" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
 ;;
 
 esac
