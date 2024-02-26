0de68dfda1e238caab1ff23f061c47017b391cfe
Luciano Moffatt <lmoffatt@gmail.com>
2024-02-26 19:09:33 -0300

    run v8 in Serafin

--- slurm_Serafin/s_MH_Li7_sim.sh
+++ slurm_Serafin/s_MH_Li7_sim.sh
@@ -34,7 +34,7 @@
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-export PATH_MACRO_DR=/home/lmoffatt/macro_dr/v6/
+export PATH_MACRO_DR=/home/lmoffatt/macro_dr/v8/
 
 # Cargar los módulos para la tarea
 module load amdblis
