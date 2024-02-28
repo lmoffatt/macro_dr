523ee7b59161ff1c4d5eec332495afc2c0770385
Luciano Moffatt <lmoffatt@gmail.com>
2024-02-28 19:34:42 -0300

    tupac run thermo for 64 cores v9

--- single_task/MH_Li_4.sh
+++ single_task/MH_Li_4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "runIdNamePre= \"MH_Li_inact_9a\""  "num_scouts_per_ensemble = get_number(n=32)"  ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
+$PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "--runIdNamePre= \"MH_Li_inact_9a\""  "--num_scouts_per_ensemble = get_number(n=32)"  ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
 
