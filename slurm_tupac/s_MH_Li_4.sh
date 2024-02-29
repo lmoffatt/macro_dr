d05ad64e53c82a90041a4fdeab65acf8bf7bab32
Luciano Moffatt <lmoffatt@gmail.com>
2024-02-28 22:53:25 -0300

    tupac run v9 bug

--- slurm_tupac/s_MH_Li_4.sh
+++ slurm_tupac/s_MH_Li_4.sh
@@ -38,4 +38,4 @@ export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export PATH_MACRO_DR=/nfs/home/lmoffatt/Code/macro_dr/v9/
 
 # Lanzar el programa
-srun $PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "--runIdNamePre= \"MH_Li_inact_9a\""  "--num_scouts_per_ensemble = get_number(n=32)"  ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
+srun $PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact.txt ../macro_dr/scripts/experiment.txt  ../macro_dr/scripts/simulation.txt "--runIdNamePre= \"MH_Li_inact_9b\""  "--num_scouts_per_ensemble = get_number(n=128)"  ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
