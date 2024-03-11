f159da1e7cf396efdd4e7602ca90dd4bfd962725
Luciano Moffatt <lmoffatt@gmail.com>
2024-03-11 08:35:03 -0300

    run idealized pulse in tupac bug

--- slurm_tupac/s_v15_4_DR.sh
+++ slurm_tupac/s_v15_4_DR.sh
@@ -35,7 +35,7 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # configurar el path
-export PATH_MACRO_DR=/nfs/home/lmoffatt/Code/macro_dr/v13/
+export PATH_MACRO_DR=/nfs/home/lmoffatt/Code/macro_dr/v15/
 
 # Lanzar el programa
 srun $PATH_MACRO_DR/macro_dr ../macro_dr/models/scheme_4_inact_NP.txt ../macro_dr/scripts/idealize_experiment.txt "--runIdNamePre= \"v15_DR_\""  "--num_scouts_per_ensemble = get_number(n=32)"  ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/evidence_thermo_data.txt
