1528a8437dbba013fb3efc51a969e568dabeb8f0
Luciano Moffatt <lmoffatt@gmail.com>
2024-03-16 17:12:06 -0300

    scripts for MHLi and MHLi_sim with continuation #149

--- slurm_tupac/run_MHLi.sh
+++ slurm_tupac/run_MHLi.sh
@@ -4,4 +4,4 @@ cd ~/Code/macro_dr/macro_dr
 
 JOBID1=$(sbatch --parsable slurm_tupac/MHLi.sh )
 
-sbatch --dependency=after:$JOBID1 slurm_tupac/MHLi_continuation.sh
+sbatch --dependency=afterany:$JOBID1 slurm_tupac/MHLi_continuation.sh
