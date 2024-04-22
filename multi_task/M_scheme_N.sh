6d070610f026fdba23200f99edee04710e8df86b
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-22 11:10:43 -0300

    new scripts for testing number cpus

--- multi_task/M_scheme_N.sh
+++ multi_task/M_scheme_N.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo $SCHEME_FILES
+
+echo $SCHEME_FILES[@]
 
 IND=$(( SLURM_LOCALID % ${#SCHEME_FILES[@]}))
 
@@ -25,7 +26,7 @@ EXPER_ABR=$([ "$EXPERIMENT" = "idealize_experiment" ] && echo "_IE" || echo "")
 
 
 
-$PATH_MACRO_DR/macro_dr ../macro_dr/models/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/scripts/simulation.txt "--runIdName= \"v21${EXPER_ABR}_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt
+${PATH_MACRO_DR}/macro_dr ../macro_dr/models/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/scripts/simulation.txt "--runIdName= \"v21${EXPER_ABR}_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt
 
 
 
