0a8667ea68d583e1e2dba2257df8d81259807fe6
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-22 10:58:13 -0300

    new scripts for testing number cpus

--- slurm_tupac/run_M_scheme_N.sh
+++ slurm_tupac/run_M_scheme_N.sh
@@ -3,15 +3,14 @@
 cd ~/Code/macro_dr/macro_dr
 
 
-
 SCHEME_FILES=( scheme_7_inact_PI  )
 
 PATH_MACRO_DRS=("/nfs/home/lmoffatt/Code/macro_dr/v21/" "/nfs/home/lmoffatt/Code/macro_dr/v22/" )
 
 EXPERIMENTS=( "idealize_experiment" )
 
-N_BETA=( 32 )
-N_SCOUTS=( 32 )
+NUM_BETAS=( 32 )
+NUM_SCOUTS=( 32 )
 
 MAX_ITERS=( 20 ) 
 
