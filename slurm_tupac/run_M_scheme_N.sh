1b300c3673247f40b350901b2aadd22f0bf39ffd
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-22 12:41:34 -0300

    new scripts for testing number cpus

--- slurm_tupac/run_M_scheme_N.sh
+++ slurm_tupac/run_M_scheme_N.sh
@@ -3,40 +3,30 @@
 cd ~/Code/macro_dr/macro_dr
 
 
-export SCHEME_FILES=( scheme_7_inact_PI  )
 
-export PATH_MACRO_DRS=("/nfs/home/lmoffatt/Code/macro_dr/v21/" "/nfs/home/lmoffatt/Code/macro_dr/v22/" )
-
-export EXPERIMENTS=( "idealize_experiment" )
-
-export NUM_BETAS=( 32 )
-export NUM_SCOUTS=( 32 )
-
-export MAX_ITERS=( 20 ) 
-
-RUNTIME="0-01:00"
+RUNTIME="0-04:00"
 NTASKS=16
 CPUSPERTASK=4
 
-JOBID1=$(sbatch --parsable --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh )
+sbatch --parsable --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh 
 
 
 NTASKS=8
 CPUSPERTASK=8
 
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh ) 
+sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
 
 NTASKS=4
 CPUSPERTASK=16
 
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh ) 
+sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
 
 NTASKS=2
 CPUSPERTASK=32
 
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh ) 
+sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
 
 NTASKS=1
 CPUSPERTASK=64
 
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh ) 
+sbatch --parsable --dependency=afterany:$JOBID1 --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh
