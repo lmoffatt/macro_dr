ca45db0fc8589f8f582efaa6613df07b2b9ad4c4
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-23 11:33:45 -0300

    test scheme_1

--- multi_task/M_scheme_N.sh
+++ multi_task/M_scheme_N.sh
@@ -36,7 +36,7 @@ EXPER_ABR=$([ "$EXPERIMENT" = "idealize_experiment" ] && echo "_IE" || echo "")
 CP=$SLURM_CPUS_PER_TASK
 
 
-if [ "$CONTINUATION" = 0 ]; then
+if [ "$CONTINUATION_NUMBER" = 0 ]; then
     ${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"tt${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt
     ;;
 else
