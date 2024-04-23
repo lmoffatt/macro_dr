f1e457a3916e389273049864c5b3ce14e2986b61
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-23 19:16:44 -0300

    run scheme_11 serafin

--- slurm_Serafin/run_M_scheme_N.sh
+++ slurm_Serafin/run_M_scheme_N.sh
@@ -17,28 +17,28 @@ module load amdlibflame
 module load gsl
 
 
-NTASKS=16
-CPUSPERTASK=4
+NTASKS=2
+CPUSPERTASK=32
 
 
 export CONTINUATION_NUMBER=0
 
-JOBID1=$(sbatch --parsable --job-name=R1_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi  slurm_Serafin/M_scheme_N.sh) 
+JOBID1=$(sbatch --parsable --job-name=R11_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi  slurm_Serafin/M_scheme_N.sh) 
 
 export CONTINUATION_NUMBER=1
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C1_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C11_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
 
 export CONTINUATION_NUMBER=2
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C1_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C11_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
 
 export CONTINUATION_NUMBER=3
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C1_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi  slurm_Serafin/M_scheme_N.sh) 
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C11_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi  slurm_Serafin/M_scheme_N.sh) 
 
 export CONTINUATION_NUMBER=4
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C1_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C11_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
 
 export CONTINUATION_NUMBER=5
-JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C1_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C11_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
 
 
 #NTASKS=8
