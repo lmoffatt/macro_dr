ac686698addfaa5941bfe450fdc5165a5c694e77
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-23 07:55:56 -0300

    speed scheme 3 9

--- slurm_tupac/run_M_scheme_N.sh
+++ slurm_tupac/run_M_scheme_N.sh
@@ -10,25 +10,25 @@ export PATH_MACRO=/nfs/home/lmoffatt/Code/macro_dr/
 
 
 
-NTASKS=16
-CPUSPERTASK=4
+#NTASKS=16
+#CPUSPERTASK=4
 
 
-sbatch --parsable --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh 
+#sbatch --parsable --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh 
 
 
-NTASKS=8
-CPUSPERTASK=8
+#NTASKS=8
+#CPUSPERTASK=8
 
-sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
 
-NTASKS=4
-CPUSPERTASK=16
+#NTASKS=4
+#CPUSPERTASK=16
 
-sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
 
 
-NTASKS=32
-CPUSPERTASK=2
+NTASKS=2
+CPUSPERTASK=32
 
 sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
