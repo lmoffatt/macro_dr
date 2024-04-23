ecf5e0e9b725c7f38a28fa01dcad60436d5ac88a
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-23 11:30:48 -0300

    test scheme_1

--- multi_task/M_scheme_N.sh
+++ multi_task/M_scheme_N.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SCHEME_FILES=( scheme_9_inact_PI  scheme_10_inact_PI  )
+SCHEME_FILES=( "models/scheme_1_inact_PI"  "models_Ag/scheme_1_inact_PI"   )
 
 PATH_MACRO_DRS=( "v23" )
 
@@ -10,7 +10,7 @@ EXPERIMENTS=( "idealize_experiment" )
 NUM_BETAS=( 32  )
 NUM_SCOUTS=( 32 )
 
-MAX_ITERS=( 20 ) 
+MAX_ITERS=( 1000 ) 
 
 IND=$(( SLURM_LOCALID % ${#SCHEME_FILES[@]}))
 
@@ -35,7 +35,16 @@ EXPER_ABR=$([ "$EXPERIMENT" = "idealize_experiment" ] && echo "_IE" || echo "")
 
 CP=$SLURM_CPUS_PER_TASK
 
-${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/models/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/scripts/simulation.txt "--runIdName= \"t${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt
+
+if [ "$CONTINUATION" = 0 ]; then
+    ${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"tt${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt
+    ;;
+else
+${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"t${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt "--continuation_number=get_number(n=${CONTINUATION_NUMBER})" ../macro_dr/scripts/evidence_thermo_continuation.txt
+   ;;
+fi
+
+
 
 
 
--- slurm_tupac/run_M_scheme_N.sh
+++ slurm_tupac/run_M_scheme_N.sh
@@ -4,31 +4,39 @@ cd ~/Code/macro_dr/macro_dr
 
 
 
-RUNTIME="0-02:00"
+RUNTIME="0-01:00"
 
 export PATH_MACRO=/nfs/home/lmoffatt/Code/macro_dr/
 
 
 
-NTASKS=16
-CPUSPERTASK=4
-
-
-sbatch --parsable --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh 
-
-
-NTASKS=8
-CPUSPERTASK=8
-
-sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
-
-NTASKS=4
-CPUSPERTASK=16
-
-sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
-
-
 NTASKS=2
 CPUSPERTASK=32
 
-sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+
+export CONTINUATION_NUMBER=0
+
+JOBID1=$(sbatch --parsable --job-name=R_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=1
+JOBID1=$(sbatch --parsable --job-name=R_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=2
+JOBID1=$(sbatch --parsable --job-name=R_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+
+#NTASKS=8
+#CPUSPERTASK=8
+
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+
+#NTASKS=4
+#CPUSPERTASK=16
+
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+
+
+#NTASKS=2
+#CPUSPERTASK=32
+
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
