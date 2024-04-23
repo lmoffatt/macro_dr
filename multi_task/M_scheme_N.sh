20afa5ff3c8ab4cf2a4a00155b97d0c8f9774c72
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-23 12:02:16 -0300

    test scheme_1

--- multi_task/M_scheme_N.sh
+++ multi_task/M_scheme_N.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-SCHEME_FILES=( "models/scheme_1_inact_PI"  "models_Ag/scheme_1_inact_PI"   )
+SCHEME_DIRS= ("models"  "models_Ag" )
+SCHEME_FILES=( "scheme_1_inact_PI"  )
 
 PATH_MACRO_DRS=( "v23" )
 
@@ -11,6 +11,10 @@ NUM_BETAS=( 32  )
 NUM_SCOUTS=( 32 )
 
 MAX_ITERS=( 20 ) 
+
+IND=$(( SLURM_LOCALID % ${#SCHEME_DIRS[@]}))
+SCHEME_DIR=${SCHEME_DIRS[$IND]}
+
 
 IND=$(( SLURM_LOCALID % ${#SCHEME_FILES[@]}))
 
@@ -37,9 +41,9 @@ CP=$SLURM_CPUS_PER_TASK
 
 
 if [ "$CONTINUATION_NUMBER" = 0 ]; then
-    ${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"tt${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt 
+    ${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME_DIR}/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"tt${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt  ../macro_dr/scripts/evidence_thermo_data.txt 
 else
-${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"t${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt "--continuation_number=get_number(n=${CONTINUATION_NUMBER})" ../macro_dr/scripts/evidence_thermo_continuation.txt 
+${PATH_MACRO}/${PATH_MACRO_DR}/macro_dr ../macro_dr/${SCHEME_DIR}/${SCHEME}.txt ../macro_dr/scripts/${EXPERIMENT}.txt  ../macro_dr/   scripts/simulation.txt "--runIdName= \"t${PATH_MACRO_DR}${EXPER_ABR}_${CP}c_${N_SCOUTS}s_${N_BETA}b_${SCHEME}_0\""  "--num_scouts_per_ensemble = get_number(n=${N_SCOUTS})" "--max_iter_equilibrium = get_number(n=${MAX_ITER})" ../macro_dr/scripts/likelihood.txt ../macro_dr/scripts/beta_${N_BETA}.txt "--continuation_number=get_number(n=${CONTINUATION_NUMBER})" ../macro_dr/scripts/evidence_thermo_continuation.txt 
 fi
 
 
