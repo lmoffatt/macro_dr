7bd6574e8e8414767ed8e8a36c2e80b5a4a154a7
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-24 17:17:06 -0300

    new scheme 15 all in #173

---
+++ slurm_Serafin/run_M_scheme_15.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd ~/macro_dr/macro_dr
+
+
+
+RUNTIME="2-00:00"
+
+export PATH_MACRO=/home/lmoffatt/macro_dr/
+
+# Cargar los módulos para la tarea
+module load cmake
+module load gcc
+
+module load amdblis
+module load amdlibflame
+module load gsl
+
+
+NTASKS=2
+CPUSPERTASK=32
+
+
+export CONTINUATION_NUMBER=0
+
+
+export SCHEME_0=scheme_15_inact_PI
+
+SCM_N=14
+
+JOBID1=$(sbatch --parsable --job-name<=R${SCM_N}_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi  slurm_Serafin/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=1
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=2
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=3
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi  slurm_Serafin/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=4
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=5
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} --partition=multi slurm_Serafin/M_scheme_N.sh) 
+
---
+++ slurm_tupac/run_M_scheme_11.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd ~/Code/macro_dr/macro_dr
+
+
+
+RUNTIME="2-00:00"
+
+export PATH_MACRO=/nfs/home/lmoffatt/Code/macro_dr/
+
+
+
+NTASKS=2
+CPUSPERTASK=32
+
+export SCHEME_0=scheme_11_inact_PI
+
+SCM_N=11
+export CONTINUATION_NUMBER=0
+
+JOBID1=$(sbatch --parsable --job-name=R${SCM_N}_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=1
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=2
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=3
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=4
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+export CONTINUATION_NUMBER=5
+JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${SCM_N}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+
+
