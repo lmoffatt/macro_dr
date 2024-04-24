34c629826596368b33a551ddbca2bb03b22cb305
Luciano Moffatt <lmoffatt@gmail.com>
2024-04-24 18:21:18 -0300

    new scheme 15 all in #173 error in standar rates

--- models_MoffattHume_allosteric.h
+++ models_MoffattHume_allosteric.h
@@ -1130,6 +1130,189 @@ static auto scheme_11_d = add_Patch_inactivation_to_model<Allost1>(
     scheme_11, 1e-5, var::MyTranformations::from_string("Log10").value());
 
 
+static auto scheme_12 = Allost1::Model("scheme_15", []() {
+    auto v_binding = Conformational_change_label{"Binding"};
+    auto v_rocking = Conformational_change_label{"Rocking"};
+    auto v_gating = Conformational_change_label{"Gating"};
+    
+    auto mo = make_Conformational_model_standarized(
+        Agonist_dependency_map{
+                               std::map<Conformational_change_label, Agonist_dependency>{
+                                                                                         {v_binding, Agonist_dependency{Agonist_label{"ATP"}}},
+                                                                                         {v_rocking, Agonist_dependency{}},{v_gating, Agonist_dependency{}}}},
+        std::vector<Conformational_change_label>{v_binding, v_rocking, v_binding,
+                                                 v_rocking, v_binding, v_rocking,v_gating},
+        std::vector<Conformational_interaction>{
+                                                {   Vector_Space{Conformational_interaction_label{"RBR"},
+                                                                 Conformational_interaction_players{
+                                                                                                    {v_rocking, v_binding, v_rocking}},
+                                                                 Conformational_interaction_positions{
+                                                                                                      {{5, 0, 1},
+                                                                                                          {1, 2, 3},
+                                                                                                          {3, 4, 5} 
+                                                                                                      }}},                Vector_Space{
+                                                                 Conformational_interaction_label{"RG"},
+                                                                 Conformational_interaction_players{{v_rocking, v_gating}},
+                                                                 Conformational_interaction_positions{{{1, 6}, {3, 6}, {5, 6}}}}
+                                                }},
+        std::vector<Conductance_interaction>{
+                                             Vector_Space{Conductance_interaction_label{"Gating_Current"},
+                                                          Conductance_interaction_players{{v_gating}},
+                                                          Conductance_interaction_positions{{{{6}}}}}},
+        std::map<Conformational_change_label, Conformation_change_standard_state>
+        {
+         
+         {v_rocking,
+          Conformation_change_standard_state{
+                                             Conformational_interactions_domain_state{std::map<
+                                                 Vector_Space<Conformational_interaction_index,
+                                                              Conformational_interaction_subposition>,
+                                                 int>{
+                                                      {Vector_Space{Conformational_interaction_index{0ul},
+                                                                    Conformational_interaction_subposition{0}},
+                                                       1},
+                                                      {Vector_Space{Conformational_interaction_index{0ul},
+                                                                    Conformational_interaction_subposition{2}},
+                                                       1}}}}},
+         {v_gating,
+          Conformation_change_standard_state{
+                                             Conformational_interactions_domain_state{std::map<
+                                                 Vector_Space<Conformational_interaction_index,
+                                                              Conformational_interaction_subposition>,
+                                                 int>{
+                                                      {Vector_Space{Conformational_interaction_index{1ul},
+                                                                    Conformational_interaction_subposition{1}},
+                                                       3},
+                                                      {Vector_Space{Conformational_interaction_index{3ul},
+                                                                    Conformational_interaction_subposition{1}},
+                                                       
+                                                       3}}}}}},
+        
+        Conductance_interaction_info(Conductance_interaction_kind::additive,
+                                     ""));
+    
+    assert(mo);
+    auto m = std::move(mo.value());
+    
+    auto names = make_ModelNames<Allost1>(m);
+    
+    auto names_vec = std::vector<std::string>{"Binding_on","Binding_off",
+                                              "Rocking_on","Rocking_off",
+                                              "Gating_on","Gating_off",
+                                              "RB","RB_0","RB_1",
+                                              "BR","BR_0","BR_1",
+                                              "RBR","RBR_0","RBR_1","RBR_2",
+                                              "RG","RG_0","RG_1",
+                                              "Gating_Current"}; //--> 8
+    
+    auto names_other = std::vector<std::string>{  "Current_Noise",    "Pink_Noise",
+                                                "Proportional_Noise", "Current_Baseline", "Num_ch"};
+    
+    
+    
+    auto p_kinetics = std::vector<double>{10.0,10000,
+                                          10000,100,
+                                          1000,100,
+                                          10,1,1,
+                                          10,1,1,
+                                          1,1,1,1,
+                                          100,10,10,
+                                          1};
+    auto p_other = std::vector<double>{1e-3, 5, 1e-2, 1, 4800};
+    
+    p_kinetics.insert(p_kinetics.end(), p_other.begin(), p_other.end());
+    auto p = Matrix<double>(p_kinetics.size(), 1, p_kinetics);
+    
+    auto tr = std::vector<std::string>(p.size(), "Log10");
+    tr[tr.size() - 2] = "Linear";
+    assert(tr.size() == p.size());
+    auto tr_param = var::MyTranformations::from_strings(tr).value();
+    
+    assert(names() == names_vec);
+    
+    names_vec.insert(names_vec.end(), names_other.begin(), names_other.end());
+    
+    auto Maybe_modeltyple_formula = make_Model_Formulas<Allost1>(m, names);
+    assert(Maybe_modeltyple_formula);
+    auto [a_Q0_formula, a_Qa_formula, a_g_formula] =
+        std::move(Maybe_modeltyple_formula.value());
+    return std::tuple(
+        [names, m](const auto &t_p)
+        -> Maybe_error<
+            Transfer_Op_to<std::decay_t<decltype(t_p)>, Patch_Model>> {
+            auto Maybe_Q0Qag = make_Model<Allost1>(m, names, t_p);
+            // std::cerr<<"parameters\n"<<p();
+            
+            assert(Maybe_Q0Qag);
+            auto [a_Q0, a_Qa, a_g] = std::move(Maybe_Q0Qag.value());
+            
+            auto Npar = names().size();
+            
+            
+            
+            auto v_curr_noise = t_p[Npar];
+            auto v_pink_noise = t_p[Npar + 1];
+            auto v_prop_noise = t_p[Npar + 2];
+            auto v_baseline = t_p[Npar + 3];
+            auto v_N0 = t_p[std::pair{Npar + 4, Npar + 4}];
+            a_g() = a_g() * -1.0;
+            
+            
+            
+            auto Nst = get<N_St>(m());
+            auto Maybe_v_P_initial = macrodr::Macro_DMR{}.calc_Pinitial(
+                a_Q0, a_Qa, ATP_concentration(0.0), Nst);
+            if (!Maybe_v_P_initial)
+                return Maybe_v_P_initial.error();
+            else
+                return build<Patch_Model>(
+                    N_St(get<N_St>(m())), std::move(a_Q0), std::move(a_Qa),
+                    std::move(Maybe_v_P_initial.value()), std::move(a_g),
+                    build<N_Ch_mean>(v_N0), build<Current_Noise>(v_curr_noise),
+                    build<Pink_Noise>(v_pink_noise),
+                    build<Proportional_Noise>(v_prop_noise),
+                    build<Current_Baseline>(v_baseline),
+                    N_Ch_mean_time_segment_duration(120000),
+                    Binomial_magical_number(5.0), min_P(1e-7),
+                    Probability_error_tolerance(1e-2),
+                    Conductance_variance_error_tolerance(1e-2));
+        },
+        
+        [names, m](const auto &patch_model)
+        -> Maybe_error<Transfer_Op_to<std::decay_t<decltype(patch_model)>,
+                                      Matrix<double>>> {
+            
+            
+            auto par = get_Parameters_from_Q0_Qa_g(
+                m, names, get<Q0>(patch_model), get<Qa>(patch_model),
+                build<g>(get<g>(patch_model)() * (-1.0)));
+            
+            if (!par)
+                return par.error();
+            auto Npar = names().size();
+            
+            auto out =
+                Transfer_Op_to<std::decay_t<decltype(patch_model)>, Matrix<double>>(
+                    Npar + 5, 1);
+            
+            out.set(std::pair(0ul, Npar - 1), par.value());
+            
+            out[Npar] = get<Current_Noise>(patch_model)();
+            out[Npar + 1] = get<Pink_Noise>(patch_model)();
+            out[Npar + 2] = get<Proportional_Noise>(patch_model)();
+            out[Npar + 3] = get<Current_Baseline>(patch_model)();
+            out.set(std::pair{Npar + 4, Npar + 4}, get<N_Ch_mean>(patch_model)());
+            return out;
+        },
+        p, names_vec, a_Q0_formula, a_Qa_formula, a_g_formula,
+        std::move(tr_param));
+});
+
+static auto scheme_12_d = add_Patch_inactivation_to_model<Allost1>(
+    scheme_12, 1e-5, var::MyTranformations::from_string("Log10").value());
+
+
+
 static auto scheme_15 = Allost1::Model("scheme_15", []() {
     auto v_binding = Conformational_change_label{"Binding"};
     auto v_rocking = Conformational_change_label{"Rocking"};
@@ -1181,7 +1364,8 @@ static auto scheme_15 = Allost1::Model("scheme_15", []() {
                                                        1},
                                                       {Vector_Space{Conformational_interaction_index{1ul},
                                                                     Conformational_interaction_subposition{1}},
-                                                       1},                          {Vector_Space{Conformational_interaction_index{2ul},
+                                                       1},
+                     {Vector_Space{Conformational_interaction_index{2ul},
                                                                     Conformational_interaction_subposition{0}},
                                                        1},
                                                       {Vector_Space{Conformational_interaction_index{2ul},
@@ -1193,9 +1377,6 @@ static auto scheme_15 = Allost1::Model("scheme_15", []() {
                                                     Vector_Space<Conformational_interaction_index,
                                                                  Conformational_interaction_subposition>,
                                                     int>{
-                                                         {Vector_Space{Conformational_interaction_index{1ul},
-                                                                       Conformational_interaction_subposition{1}},
-                                                          3},
                                                          {Vector_Space{Conformational_interaction_index{3ul},
                                                                        Conformational_interaction_subposition{1}},
                                                           
