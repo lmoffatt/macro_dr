13f88554b1193e3108746a4e6092169469f1f5e5
Luciano Moffatt <lmoffatt@gmail.com>
2024-05-17 10:28:57 -0300

    dedicated scripts for each scheme combinations

---
+++ slurm_tupac/run_M_schemes_9_10.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+cd ~/Code/macro_dr/macro_dr
+
+
+
+RUNTIME="2-00:00"
+
+export PATH_MACRO=/nfs/home/lmoffatt/Code/macro_dr/
+
+export EVIDENCE_ALGORITHM=levenberg
+
+
+export NTASKS=4
+CPUSPERTASK=16
+export USE_LOCAL_ID=0
+
+N_SCH=9
+N_SCH2=10
+
+export SCHEME_DIR_0=models
+export SCHEME_DIR_1=models_Ag
+export SCHEME_DIR_2=models
+export SCHEME_DIR_3=models_Ag
+
+export SCHEME_0=scheme_${N_SCH}_inact_PI
+export SCHEME_1=scheme_${N_SCH}_inact_PI
+export SCHEME_2=scheme_${N_SCH2}_inact_PI
+export SCHEME_3=scheme_${N_SCH2}_inact_PI
+
+
+export PATH_MACRO_DR_0=v30
+export PATH_MACRO_DR_1=$PATH_MACRO_DR_0
+export PATH_MACRO_DR_2=$PATH_MACRO_DR_0
+export PATH_MACRO_DR_3=$PATH_MACRO_DR_0
+
+export PATH_MACRO_DR=v30
+
+export PATH_MACRO_DRX=v30
+
+
+export EXPERIMENT_0=idealize_experiment_2
+export EXPERIMENT_1=$EXPERIMENT_0
+export EXPERIMENT_2=$EXPERIMENT_0
+export EXPERIMENT_3=$EXPERIMENT_0
+
+
+export N_BETA=32
+
+export N_SCOUTS=32
+
+export MAX_ITER=1000000
+
+
+
+JOBID1=9097
+
+
+export CONTINUATION_NUMBER=0
+
+JOBID1=$(sbatch --parsable --job-name=R${N_SCH}_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N.sh) 
+
+#export CONTINUATION_NUMBER=1
+#JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${N_SCH}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N_tasks.sh) 
+
+#export CONTINUATION_NUMBER=2
+#JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${N_SCH}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N_tasks.sh) 
+
+#export CONTINUATION_NUMBER=3
+#JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${N_SCH}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N_tasks.sh) 
+
+#export CONTINUATION_NUMBER=4
+#JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${N_SCH}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N_tasks.sh) 
+
+#export CONTINUATION_NUMBER=5
+#JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${N_SCH}_${CPUSPERTASK}_${CONTINUATION_NUMBER} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  slurm_tupac/M_scheme_N_tasks.sh) 
+
+#NTASKS=8
+#CPUSPERTASK=8
+
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK} --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+
+#NTASKS=4
+#CPUSPERTASK=16
+
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
+
+
+#NTASKS=2
+#CPUSPERTASK=32
+
+#sbatch --parsable  --job-name=T_${CPUSPERTASK} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME} slurm_tupac/M_scheme_N.sh 
