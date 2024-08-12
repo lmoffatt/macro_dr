18fa00865fc6249003af5681eca42d9fbea705c1
Luciano Moffatt <lmoffatt@gmail.com>
2024-08-12 20:03:18 -0300

    run different likelihoods adr,dr,r and nr #242

--- slurm_Serafin/run_Lik_schemes_10_11.sh
+++ slurm_Serafin/run_Lik_schemes_10_11.sh
@@ -84,7 +84,7 @@ export PATH_MACRO_DR_14=$PATH_MACRO_DR_0
 export PATH_MACRO_DR_14=$PATH_MACRO_DR_0
 
 
-export PATH_MACRO_DRX=w1
+export PATH_MACRO_DRX=w3
 
 
 export EXPERIMENT_0=idealize_experiment_2
