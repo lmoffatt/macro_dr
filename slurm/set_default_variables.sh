99e0a77b80998c1a0751c0396a3ffb1c69cab4e4
Luciano Moffatt <lmoffatt@gmail.com>
2024-08-15 12:14:40 -0300

    scripts

--- slurm/set_default_variables.sh
+++ slurm/set_default_variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export WORKING_DIRECTORY=data_CPU1
+export WORKING_DIRECTORY=data_CPU5
 
-export RUNTIME=0-00:30
+export RUNTIME=3-00:00
 export EVIDENCE_ALGORITHM=thermo_dts
 export PATH_MACRO_DRX=w5
-export MAX_ITER=1000
+export MAX_ITER=1000000
