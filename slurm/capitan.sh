c5d5256e37c499a6f5e68d7aad74134dabe97a1f
Luciano Moffatt <lmoffatt@gmail.com>
2024-08-15 14:57:00 -0300

    capitan

--- slurm/capitan.sh
+++ slurm/capitan.sh
@@ -4,12 +4,14 @@ cd ~/Code/macro_dr/macro_dr
 
 module use /opt/ohpc/pub/apps/modules/all/
 module load cmake
-module load GSL
-module load OpenBLAS
-module load GCC
 export CC=$HOME/local/gcc-14.2.0/bin/gcc
 export CXX=$HOME/local/gcc-14.2.0/bin/g++
+module load GSL
+module load OpenBLAS
+
 export PATH_MACRO=${HOME}/Code/macro_dr/
+
+
 export PARTITION=eth_hi
 
 
