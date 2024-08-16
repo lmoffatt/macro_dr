2155c6099ecebf61a35e01108927ef0cb1fc01b0
Luciano Moffatt <lmoffatt@gmail.com>
2024-08-16 11:18:38 -0300

    changes in script logic

--- clusters/capitan.sh
+++ clusters/capitan.sh
@@ -5,8 +5,8 @@ cd ~/Code/macro_dr/macro_dr
 module use /opt/ohpc/pub/apps/modules/all/
 module load cmake
 module load git
-module load intel/2022.0.2
 module load GSL/2.7-GCC-12.2.0module load GSL
+module load intel/2022.0.2
 module load OpenBLAS
 
 export PATH_MACRO=${HOME}/Code/macro_dr
