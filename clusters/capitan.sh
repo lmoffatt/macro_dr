53bd2fb2ef179c62a3d356ef83053c3e60cdace4
Luciano Moffatt <lmoffatt@gmail.com>
2024-08-16 11:30:54 -0300

    changes in script logic

--- clusters/capitan.sh
+++ clusters/capitan.sh
@@ -3,7 +3,7 @@
 cd ~/Code/macro_dr/macro_dr
 
 module use /opt/ohpc/pub/apps/modules/all/
-module load GSL/2.7-GCC-12.2.0module load GSL
+module load GSL/2.7-GCC-12.2.0
 module load intel/2022.0.2
 
 export PATH_MACRO=${HOME}/Code/macro_dr
