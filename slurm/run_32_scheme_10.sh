338d0f75f95f5c842b42b561764ae1897acf2a1a
Luciano Moffatt <lmoffatt@gmail.com>
2024-10-04 17:46:19 -0300

    replica scheme10

--- slurm/run_32_scheme_10.sh
+++ slurm/run_32_scheme_10.sh
@@ -18,7 +18,7 @@ export SCHEME_1=scheme_${N_SCH}_inact_PI
 export SCHEME_DIR_0=models_Ag
 export SCHEME_DIR_1=models_Ag
 
-export PATH_MACRO_DR_0=w5
+export PATH_MACRO_DR_0=w5b
 export PATH_MACRO_DR_1=$PATH_MACRO_DR_0
 
 
