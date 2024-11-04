f172ea87c1bc3e3da604210ef4726cd3a7115296
Luciano Moffatt <lmoffatt@gmail.com>
2024-11-04 08:32:54 -0300

    new bessel run

--- slurm/run_32_scheme_10_bassel.sh
+++ slurm/run_32_scheme_10_bassel.sh
@@ -18,7 +18,7 @@ export SCHEME_1=scheme_${N_SCH}_inact_PI
 export SCHEME_DIR_0=models_Ag
 export SCHEME_DIR_1=models_Ag
 
-export PATH_MACRO_DR_0=w5_bessel2_
+export PATH_MACRO_DR_0=w5_bessel_
 export PATH_MACRO_DR_1=$PATH_MACRO_DR_0
 
 
@@ -45,12 +45,12 @@ export MAX_ITER=1000000
 
 
 
-export CONTINUATION_NUMBER=0
+export CONTINUATION_NUMBER=1
 
-JOBID1=$(sbatch --parsable --job-name=R${N_SCH}_${CPUSPERTASK}  --partition=${PARTITION} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  ${PATH_MACRO}/macro_dr/slurm/M_scheme_N_tasks.sh) 
+JOBID1=$(sbatch --parsable --job-name=R${C_SCH}_${CPUSPERTASK}  --partition=${PARTITION} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  ${PATH_MACRO}/macro_dr/slurm/M_scheme_N_tasks.sh) 
 
 
-for i in $(seq 1 8);
+for i in $(seq 2 8);
 do
     export CONTINUATION_NUMBER=$i
     JOBID1=$(sbatch --parsable --dependency=afterany:$JOBID1 --job-name=C${N_SCH}_${CPUSPERTASK}_${CONTINUATION_NUMBER}   --partition=${PARTITION} --ntasks-per-node=${NTASKS} --cpus-per-task=${CPUSPERTASK}  --time=${RUNTIME}  ${PATH_MACRO}/macro_dr/slurm/M_scheme_N_tasks.sh) 
