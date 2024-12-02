4e0bea3b21a03720b37199736c6d5353b6197bf4
Luciano Moffatt <lmoffatt@gmail.com>
2024-12-02 11:48:06 -0300

    w6 directory

--- slurm/run_32_scheme_10_NR.sh
+++ slurm/run_32_scheme_10_NR.sh
@@ -18,7 +18,7 @@ export SCHEME_1=scheme_${N_SCH}_inact_PI
 export SCHEME_DIR_0=models_Ag
 export SCHEME_DIR_1=models_Ag
 
-export PATH_MACRO_DR_0=w5_NR_
+export PATH_MACRO_DR_0=w6_NR_
 export PATH_MACRO_DR_1=$PATH_MACRO_DR_0
 
 
@@ -28,6 +28,7 @@ export PATH_MACRO_DR_1=$PATH_MACRO_DR_0
 export LIK_0=NR
 export LIK_1=$LIK_0
 
+export PATH_MACRO_DRX=w6
 
 
 
