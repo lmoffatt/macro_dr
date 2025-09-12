60db285cddf2ebd32420937cafc46d9434d28265
Luciano Moffatt <lmoffatt@gmail.com>
2025-09-12 09:44:49 -0300

    dirac cluster

--- clusters/dirac.sh
+++ clusters/dirac.sh
@@ -3,9 +3,9 @@
 cd ~/Code/macro_dr/macro_dr
 
 
-module load gnu14
-module load openblas/0.3.28
 module load gsl
+module load gnu14
+#module load openblas/0.3.28
 export PATH_MACRO=${HOME}/Code/macro_dr
 export CLUSTER=dirac
 
